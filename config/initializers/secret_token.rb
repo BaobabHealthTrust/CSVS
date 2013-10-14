5623cbf2c69437dbacd8b49e170ed3e419f218e5
catchit <precious.bondwe@googlemail.com>
2013-10-14 17:20:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSVS::Application.config.secret_token = 'ff2a673f604b9ad2b050ab14755b5c171a65c685d09fc50dca6fe3d1cf5423a2c824c778f771fb4d7d4fb6e2f79ab0c791aced23f0a2fd99c54171f438d2b190'
