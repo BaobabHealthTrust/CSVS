db29ba2b74fcf67d1af2477eb0ce59c779201876
exilenjoka <exilenjoka@exilenjoka-Aspire-E1-531.(none)>
2013-11-01 13:21:21 +0200

    added report dept  mechanism

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,63 +2,33 @@ class ReportController < ApplicationController
 	
 	 def show
 	  @service = Service.all.delete_if{|service|service.name.blank?}
-		render :get_report_specs
+		render :choose_report
 	 end
    
      def get_report_specs
 
-	   @service = Service.all.delete_if{|service|service.name.blank?}
-              
-     end
-  
-      def display_report
-
-		 @dept = params[:dept]	
-   		 dept = Service.find_by_name(params[:dept]).id
-    	 period = params[:period]
-	 
-		 case period
-			when "DAY"
-			@total 		   = Vote.where(["service_id=? AND created_at >=?",dept,Time.now-1.day]).count
-           
-			@fullysatified =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											1.day,17]).count 
-            @satisfies     =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											1.day,18]).count 
-			@atleast	   =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											1.day,19]).count 
-			@abit          =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											1.day,20]).count 
-			@notsat        =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 			
-								1.day,21]).count 
-		 when "WEEK"
-			@total 		   = Vote.where(["service_id=? AND created_at >=?",dept,Time.now-7.day]).count			
-			
-			@fullysatified =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											7.day,17]).count 
-            @satisfies     =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											7.day,18]).count 
-			@atleast	   =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											7.day,19]).count 
-			@abit          =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											7.day,20]).count 
-			@notsat        =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 			
-								7.day,21]).count
-		when "MONTH" 
-			@total 		   = Vote.where(["service_id=? AND created_at >=?",dept,Time.now-30.day]).count
-			
-			@fullysatified =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											30.day,17]).count 
-            @satisfies     =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											30.day,18]).count 
-			@atleast	   =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											30.day,19]).count 
-			@abit          =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											30.day,20]).count 
-			@notsat        =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 			
-								30.day,21]).count
-       else
-			@total 		   = Vote.where(["service_id=? AND created_at >=?",dept,Time.now-365.day]).count
+		@service=params[:service]
+		service = Service.find_by_name(params[:service]).id
+		start_date = Date.parse(params[:start_date])
+		end_date =  Date.parse(params[:end_date])
 		
-			@fullysatified =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											365.day,17]).count 
-            @satisfies     =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											365.day,18]).count 
-			@atleast	   =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											365.day,19]).count 
-			@abit          =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 											365.day,20]).count 
-			@notsat        =Vote.where(["service_id=? AND created_at >=? AND vote_type_id=?",dept,Time.now- 			
-								365.day,21]).count
-		end
+		full_id = VoteType.find_by_name("FULLY SATISFIED").id
+		sat_id = VoteType.find_by_name("SATISFIED").id
+		atleast_id = VoteType.find_by_name("ATLEAST SATISFIED").id
+		abit_id = VoteType.find_by_name("A BIT SATISFIED").id
+		unsat_id = VoteType.find_by_name("NOT SATISFIED").id
 		
-
-	end
-
-	
-	
+		@total = Vote.where(["service_id=? AND DATE(created_at)>=? AND Date(created_at)<=?",
+										service,start_date,end_date]).count
+        @fullysatisfied = Vote.where(["service_id=? AND DATE(created_at)>=? AND Date(created_at)<=? AND vote_type_id=?",
+										service,start_date,end_date,full_id]).count
+		@satisfied = Vote.where(["service_id=? AND DATE(created_at)>=? AND Date(created_at)<=? AND vote_type_id=?",
+										service,start_date,end_date,sat_id]).count
+	   @atleast= Vote.where(["service_id=? AND DATE(created_at)>=? AND Date(created_at)<=? AND vote_type_id=?",
+										service,start_date,end_date,atleast_id]).count
+	    @abit = Vote.where(["service_id=? AND DATE(created_at)>=? AND Date(created_at)<=? AND vote_type_id=?",
+										service,start_date,end_date,abit_id]).count
+	    @notsat= Vote.where(["service_id=? AND DATE(created_at)>=? AND Date(created_at)<=? AND vote_type_id=?",
+										service,start_date,end_date,unsat_id]).count
+	     end	
 end
