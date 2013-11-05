89e9e47e284220b3d6b45b18067ec371de9783b0
exilenjoka <exilenjoka@exilenjoka-Aspire-E1-531.(none)>
2013-11-05 15:07:24 +0200

    added default values in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+puts 'loading Vote_types'
+vote_types = ["FULLY SATISFIED","SATISFIED","ATLEAST SATISFIED","A BIT SATISFIED","NOT SATISFIED"]
+  			 (vote_types).each do |name|
+  			 v_type = VoteType.new()
+ 			 v_type.name = name
+ 			 v_type.save
+	      end
+
+puts 'loading services'
+service_types = ["OPD","Martenity","Theatre","Eye","Dental","Skin Clinic","OB/Gyn",	 "Casualties",
+				 "Medical","Pediatrics","Family Planning","Surgical","Orthropedics","Radiology"]
+  			 (service_types).each do |name|
+  			 s_type = Service.new()
+ 			 s_type.name = name
+ 			 s_type.save
+	      end
+
+puts 'loading issues'
+ issues = ["No Medicine","No security","Harsh Attitude Nurses","Unhygienic rooms","Unhygienic bedings","Delay treatment",
+			"Careless Nurses"
+			]
+  			 (issues).each do |name|
+  			 is_type = Issue.new()
+ 			 is_type.name = name
+ 			 is_type.save
+	      end
